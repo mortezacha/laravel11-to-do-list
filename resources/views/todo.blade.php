3888ce1352851402afc34fd92c649de92026b1e8
mori_cha <mortezachabooshi@gmail.com>
2024-05-22 23:23:35 +0430

    changed form margin bottom to margin y

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -20,7 +20,7 @@
 
         <form class="max-w-md mx-auto mb-5" method="POST" action="/create-todo">
             @csrf
-            <div class="relative z-0 w-full mb-5 group">
+            <div class="relative z-0 w-full my-5 group">
                 <input type="text" name="text" id="text" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                 <label for="text" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Add New Task:</label>
             </div>
@@ -81,7 +81,7 @@
                             <!-- Modal header -->
                             <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                                 <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                                    Delete task from list
+                                    Check this item
                                 </h3>
                                 <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="check-modal-{{$todo->id}}">
                                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
